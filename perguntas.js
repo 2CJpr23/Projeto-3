c71a7b7313a696ba27e5a9fcac310e90e25f4a51
2CJpr23 <00001108063482sp@al.educacao.sp.gov.br>
2024-11-08 23:59:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Eua?',
+    'Washington'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilía'
+)
+
+criaCartao(
+    'História',
+    'Quem colonizou o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Ed Financeira',
+    'Como faz para ficar rico sem trabalhar e sem esforço?',
+    'Também queria saber :('
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Senõr ou Don'
+)
+
+criaCartao(
+    'Espanhol',
+    'Como se fala Senhor em Espanhol ?',
+    'Oi em ingles é HI (RAI)'
+)
+
+
+criaCartao(
+    'Inglês',
+    'Como se fala pai em Inglês?',
+    'Dad ou Father'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se fala cachorro em Inglês?',
+    'Cachorro se chama dog'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem ao todo?',
+    ' O planeta Terra tem seis continentes: África, América, Antártida, Ásia, Europa e Oceania. '
+)
